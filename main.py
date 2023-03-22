f9949fe046fdd25bbe335cd90116de6184f813f7
TDPhantom <abattlessmith@brooklynsteamcenter.org>
2023-03-22 10:24:16 -0400

    add

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask , render_template, request, redirect
-#from flask_httpauth import HTTPBasicAuth
-#from werkzeug.security import generate_password_hash, check_password_hash
-#import pymysql
-#import pymysql.cursors
+
 
 app = Flask(__name__)
+
+@app.route("/")
 def index():
     print("HazyTDP Testing Testing")
+    return"HazyTDP Testing Testing"

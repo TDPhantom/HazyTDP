9a11a9ac9055b7665928fb9c5cb16571fc4c1c3e
TDPhantom <abattlessmith@brooklynsteamcenter.org>
2023-03-22 10:13:45 -0400

    new

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask , render_template, request, redirect
+#from flask_httpauth import HTTPBasicAuth
+#from werkzeug.security import generate_password_hash, check_password_hash
+#import pymysql
+#import pymysql.cursors
+
+app = Flask(__name__)
+def index():
+    print("HazyTDP Testing Testing")

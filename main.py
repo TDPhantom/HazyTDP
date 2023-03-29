9bc75c3712562ef02628bfa3e0d175d3ece6fd65
TDPhantom <abattlessmith@brooklynsteamcenter.org>
2023-03-29 10:55:17 -0400

    added

--- main.py
+++ main.py
@@ -30,6 +30,33 @@ def post_feed():
 
     return render_template("posts.html.jinja",posts=results )
 
+@app.route('/sign-in')
+def sign_in():
+    return render_template("sign_in.html.jinja")
+
+@app.route('/sign-up', methods=['POST', 'GET'])
+def sign_up():
+    if request.method =='POST':
+        #Handle signup
+        cursor = connection.cursor()
+
+        cursor.execute("""
+            INSERT INTO `users` (`username`, `display_name`,`email`,`birthday`,`password`,`bio`,`photo)
+            VALUES(%s,%s,%s,%s,%s,%s,%s,)
+        """,[])
+        
+        
+        return request.form
+    elif request.method == 'GET':
+        return render_template("Sign_up.html.jinja")
+    
+        
+
+
+
+    return render_template("sign_up.html.jinja")
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)

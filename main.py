51b579c6befbe47e34fea4607c1d004c097c45ba
TDPhantom <abattlessmith@brooklynsteamcenter.org>
2023-03-30 10:56:07 -0400

    added

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from flask import Flask , render_template, request, redirect
 import pymysql
 import pymysql.cursors
+from flask_login import LoginManager
+
+login_manager = LoginManager
 
 app = Flask(__name__)
+login_manager.init_app(app)
 
 @app.route("/")
 def index():
     return render_template("home.html.jinja")
 
+class User:
+    def __init__(self, id, Username, banned):
+        self.is_authenticated = True
+        self.is_anonymous = False
+        self. is_active = not banned
+
+        self.user = Username
+        self.id = id
+
+    def get_id(self):
+        return str(self.id)
+    
+
+    
 
 connection = pymysql.connect(
     host="10.100.33.60",
@@ -17,8 +35,18 @@ connection = pymysql.connect(
     cursorclass=pymysql.cursors.DictCursor,
     autocommit=True,
     )
+@login_manager.user_loader
+def user_loader(user_id):
+    cursor = connection.cursor()
 
+    cursor.execute("SELECT * from `Users` WHERE `id` = " + user_id)
 
+    result = cursor.fetchone()
+
+    if result is None:
+        return None
+    
+    return User(result['id'], result['Username'], result['banned'])
 
 @app.route('/post')
 def post_feed():
@@ -40,15 +68,35 @@ def sign_up():
         #Handle signup
         cursor = connection.cursor()
 
+        Profile =request.files['Profile']
+       
+        file_name = Profile.filename # my_jgp
+        
+        file_extension = file_name.split('.')[-1]
+
+        if file_extension in ['jpg','jpeg', 'png', 'gif']:
+            Profile.save('media/users' + file_name)
+
+        else:
+            raise Exception('Invalid file type')
+       
+       
+       
+       
         cursor.execute("""
-            INSERT INTO `users` (`username`, `display_name`,`email`,`birthday`,`password`,`bio`,`photo)
-            VALUES(%s,%s,%s,%s,%s,%s,%s,)
-        """,[])
+            INSERT INTO `Users` (`username`, `display_name`,`email`,`Birthday`,`password`,`bio`,`photo`)
+            VALUES(%s,%s,%s,%s,%s,%s,%s)
+        """,(request.form['Username'], request.form['Display Name'], request.form['Email'], request.form['Birthday'], request.form['Password'], request.form['Biography'], file_name))
+
+
+        return redirect('/posts')
+    elif request.method =='GET':
+        return render_template('sign_up.html.jinja')
         
         
         return request.form
     elif request.method == 'GET':
-        return render_template("Sign_up.html.jinja")
+        return render_template("sign_up.html.jinja")
     
         
 
